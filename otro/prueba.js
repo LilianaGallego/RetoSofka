bd35d90f5118d9825e477e80d4308d1a102d8f19
LilianaGallego <lilianagallegom@gmail.com>
2022-04-28 15:13:37 -0500

    commit final

--- otro/prueba.js
+++
@@ -1,9 +0,0 @@
-function mostrar() {
-    var x = document.getElementById('img').style.display='block';
-  
-}
-
-function ocultar() {
-var x = document.getElementById('img').style.display='none';
-
-}
